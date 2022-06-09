b3b2fae4e5ef616476e5dcbd8f18c26c9164ea63
TPTan96 <tptan12345@gmail.com>
2022-06-09 18:19:20 +0700

    init project

---
+++ backend/src/businessLogic/todos.ts
@@ -0,0 +1,56 @@
+import { TodosAccess } from '../dataLayer/todosAcess'
+import { AttachmentUtils } from '../fileStorage/attachmentUtils';
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { TodoUpdate } from '../models/TodoUpdate'
+// import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid'
+// import * as createError from 'http-errors'
+
+
+
+// TODO: Implement businessLogic
+const todosAccess = new TodosAccess();
+const accessFile = new AttachmentUtils();
+
+export async function createAttachmentPresignedUrl(userId: string, todoId: string): Promise<String> {
+    const uploadUrl = await accessFile.getUploadUrl(todoId);
+    const attachmentUrl = accessFile.getAttachmentUrl(todoId);
+    await todosAccess.updateAttachmentUrl(userId, todoId, attachmentUrl);
+    return uploadUrl;
+}
+
+export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
+    return todosAccess.getTodosForUser(userId);
+}
+
+export async function createTodo(createTodoRequest: CreateTodoRequest, userId: string): Promise<TodoItem> {
+
+    const todoId = uuid.v4();
+    const timestamp = new Date().toISOString();
+
+    return await todosAccess.createTodo({
+        userId: userId,
+        todoId: todoId,
+        createdAt: timestamp,
+        name: createTodoRequest.name,
+        dueDate: createTodoRequest.dueDate,
+        done: false
+    });
+}
+
+export async function updateTodo(todoId: string, updateTodoRequest: UpdateTodoRequest, userId: string): Promise<TodoUpdate> {
+
+    return await todosAccess.updateTodo({
+        name: updateTodoRequest.name,
+        dueDate: updateTodoRequest.dueDate,
+        done: updateTodoRequest.done
+    },
+        todoId,
+        userId);
+}
+
+export async function deleteTodo(todoId: string, userId: string) {
+    await todosAccess.deleteTodo(todoId, userId)
+}
---
+++ backend/src/fileStorage/attachmentUtils.ts
@@ -0,0 +1,34 @@
+import * as AWS from 'aws-sdk'
+// import * as AWSXRay from 'aws-xray-sdk'
+
+// const XAWS = AWSXRay.captureAWS(AWS)
+
+export class AttachmentUtils {
+    
+    constructor(
+        private readonly s3: AWS.S3 = createS3Client(),
+        private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
+        private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION) {
+    }
+
+    async getUploadUrl(todoId: string): Promise<string> {
+        const url =this.s3.getSignedUrl('putObject', {
+            Bucket: this.bucketName,
+            Key: todoId,
+            Expires: parseInt(this.urlExpiration)
+        })
+        return url;
+    }
+
+    getAttachmentUrl(todoId: string): string {
+        return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`;
+    }
+}
+
+function createS3Client() : AWS.S3 {
+    
+    const s3 = new AWS.S3({
+        signatureVersion: 'v4'
+    });
+    return s3;
+}
